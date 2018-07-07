127a3fe768c81d45d073a69a236527f06c8c6e8e
admin <admin@192.168.0.10>
2018-07-06 22:17:23 -0500

    commit con issues solucionados

--- src/com/bancodebogota/fdm/proyectorestaurante/IngredienteLiquido.java
+++ src/com/bancodebogota/fdm/proyectorestaurante/IngredienteLiquido.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyectococina;
+package com.bancodebogota.fdm.proyectorestaurante;
 
 /**
  *
